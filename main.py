91d69aaaa9752755f7b163afc628b7713c12dd40
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2020-03-22 21:03:58 +0000

    extracted message listener to separate function

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ from rpi_rf import RFDevice
 
 gpio_pin = 26  # RF receiver is connected to pin 37 (GPIO26)
 
+def listen_for_messages():
+    timestamp = None
+    if rfdevice.rx_code_timestamp != timestamp:
+        timestamp = rfdevice.rx_code_timestamp
+        print(str(rfdevice.rx_code) +
+                " [pulselength " + str(rfdevice.rx_pulselength) +
+                ", protocol " + str(rfdevice.rx_proto) + "]")
+    time.sleep(0.01)
+
 if __name__ == "__main__":
     try:
         rfdevice = RFDevice(gpio_pin)
         rfdevice.enable_rx()
-        timestamp = None
-        print("Listening for codes on GPIO " + str(gpio_pin))
 
+        print("Listening for codes on GPIO " + str(gpio_pin))
         while True:
-            if rfdevice.rx_code_timestamp != timestamp:
-                timestamp = rfdevice.rx_code_timestamp
-                print(str(rfdevice.rx_code) +
-                      " [pulselength " + str(rfdevice.rx_pulselength) +
-                      ", protocol " + str(rfdevice.rx_proto) + "]")
-            time.sleep(0.01)
+            listen_for_messages()
+
 
     except KeyboardInterrupt:
         rfdevice.cleanup()
