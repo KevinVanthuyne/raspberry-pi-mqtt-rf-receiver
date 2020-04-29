e7fa0cd26e0bb0f71c0787dcc19a00d49bb9b719
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2020-04-29 21:39:03 +0200

    updated Arduino sketch

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -1,20 +1,29 @@
+#include "LowPower.h" // https://github.com/rocketscream/Low-Power
 #include <RCSwitch.h>
 
-#define BUTTON_PIN 3
+#define BUTTON_PIN 2
 #define RF_PIN 10
 
 RCSwitch mySwitch = RCSwitch();
+
+void buttonPressed() {
+  // Empty handler
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   mySwitch.enableTransmit(RF_PIN);
+  Serial.println("Setup complete");
+  delay(200); // necessary to do setup before going to sleep
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    mySwitch.send(2201, 24);
-    Serial.println("button pressed");
-    delay(500);  
-  }
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressed, LOW);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+  detachInterrupt(digitalPinToInterrupt(BUTTON_PIN)); 
+
+  mySwitch.send(2201, 24);
+  Serial.println("RF message sent");
+  delay(200); // delay necessary to prevent Arduino from going to sleep before finishing print
 }
