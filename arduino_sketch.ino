c467f0b49319924d696d8db971c62df808e8c6d3
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2020-04-30 20:47:21 +0200

    added battery voltage to rf message

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -1,10 +1,12 @@
 #include "LowPower.h" // https://github.com/rocketscream/Low-Power
 #include <RCSwitch.h>
+#include <VoltageReference.h>
 
 #define BUTTON_PIN 2
 #define RF_PIN 10
 
 RCSwitch mySwitch = RCSwitch();
+VoltageReference voltRef;
 
 void buttonPressed() {
   // Empty handler
@@ -14,6 +16,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   mySwitch.enableTransmit(RF_PIN);
+  voltRef.begin();
   Serial.println("Setup complete");
   delay(200); // necessary to do setup before going to sleep
 }
@@ -23,7 +26,8 @@ void loop() {
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
   detachInterrupt(digitalPinToInterrupt(BUTTON_PIN)); 
 
-  mySwitch.send(2201, 24);
+  int vcc = voltRef.readVcc(); // battery voltage in mV
+  mySwitch.send(22010000 + vcc, 32); // 2201 (message code) + vcc value
   Serial.println("RF message sent");
   delay(200); // delay necessary to prevent Arduino from going to sleep before finishing print
 }
