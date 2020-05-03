ac8d46381f09e374f1b2223d9a584497bdb85eaa
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2020-05-03 16:14:49 +0200

    refactored to use logging module

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import os
 import datetime
 import paho.mqtt.client as mqtt
+import logging
 from rpi_rf import RFDevice
 from dotenv import load_dotenv
 
@@ -12,15 +13,18 @@ gpio_pin = 26  # RF receiver is connected to pin 37 (GPIO26)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("MQTT CONNECT: Connected with result code "+str(rc))
+    logging.info("MQTT CONNECT: Connected with result code %s", str(rc))
 
 def on_log(client, userdata, level, buf):
-    print("MQTT LOG: client {} - userdata {} - level {} - bug {}".format(client, userdata, level, bug))
+    logging.info("MQTT LOG: client %s - userdata %s - level %s - bug %s", client, userdata, level, bug)
 
 if __name__ == "__main__":
     try:
         # load environment variables
         load_dotenv()
+
+        # setup logger
+        logging.basicConfig(format = "%(asctime)s %(levelname)-10s %(message)s", level = logging.INFO)
 
         # setup mqtt
         client = mqtt.Client("rf_client")
@@ -34,25 +38,26 @@ if __name__ == "__main__":
         rfdevice = RFDevice(gpio_pin)
         rfdevice.enable_rx()
 
+
         # start listening for rf messages
-        print("Listening for codes on GPIO " + str(gpio_pin))
+        logging.info("Listening for codes on GPIO " + str(gpio_pin))
         timestamp = None
 
         while True:
             if rfdevice.rx_code_timestamp != timestamp:
                 timestamp = rfdevice.rx_code_timestamp
 
-                print("[{}] {} (pulselength {}, protocol {})".format(
-                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                logging.info(
+                    "Received message: %s (pulselength %s, protocol %s)", 
                     rfdevice.rx_code,
                     rfdevice.rx_pulselength,
                     rfdevice.rx_proto
-                ))
+                )
 
                 # first 4 numbers contain "id" code, next 4 numbers contain voltage of the sender's battery in mV
                 if str(rfdevice.rx_code)[:4] == "2201":
                     client.publish("rf_button_1", str(rfdevice.rx_code)[4:8])
-                    print("Published on 'rf_button_1' topic with '{}' as message".format(str(rfdevice.rx_code)[4:8]))
+                    logging.info("  Published 'rf_button_1' topic with message '%s'", str(rfdevice.rx_code)[4:8])
                     time.sleep(1) # debounce/throttle signal
                     timestamp = rfdevice.rx_code_timestamp # update timestamp after wait to prevent looping a second time
 
